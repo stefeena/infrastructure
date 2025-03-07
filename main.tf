45d77a7942da032de0634d1843dbbb7b759c5957
stefeena <stefeenabenny@gmail.com>
2025-03-06 20:02:09 -0500

    Fix Terraform syntax and remove invalid shell commands

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     azurerm = {
@@ -6,7 +5,6 @@ terraform {
       version = ">=2.0"
     }
   }
-
   required_version = ">= 1.2.4"
 }
 
@@ -18,8 +16,3 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "East US"
 }
-# Add pre-commit hooks for Terraform checks
-echo "terraform fmt -check -recursive" > .husky/pre-commit
-echo "terraform validate" >> .husky/pre-commit
-echo "tflint" >> .husky/pre-commit
-chmod +x .husky/pre-commit
